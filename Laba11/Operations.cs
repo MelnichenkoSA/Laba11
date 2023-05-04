efcbd02f161388fc9180f86e883119e892924b2d
melnichenkosa <melnichenkosa@302D-5>
2023-05-04 16:02:12 +0700

    Добавьте файлы проекта.

---
+++ Laba11/Operations.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laba11
+{
+    class Operations
+    {
+        delegate double Message(double a);
+        Message[] messages = { Plus, Minus, Umnozh, Podelit };
+        static double Plus(double a)
+        {
+            return a + a;
+        }
+        static double Minus(double a)
+        {
+            return a - 2;
+        }
+        static double Umnozh(double a)
+        {
+            return a * a;
+        }
+        static double Podelit(double a)
+        {
+            return a / 2;
+        }
+        public void Print(double a)
+        {
+            foreach (var item in messages)
+            {
+                a = item(a);
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
---
+++ Laba11/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Laba11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operations op = new Operations();
+            Console.WriteLine("Введите число");
+            double a = double.Parse(Console.ReadLine());
+            op.Print(a);
+        }
+    }
+}
